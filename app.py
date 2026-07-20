36d791774162a506a9b0ee0cc723ebf24a526619
Pedro Vittor <vittorp509@gmail.com>
2026-07-20 00:56:44 -0300

    bbb

--- app.py
+++ app.py
@@ -147,6 +147,7 @@ if salvar:
         str(linha["feedback"]),
         str(avaliacao),
         str(observacao),
+        datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     ])
 
     st.session_state.indice += 1
